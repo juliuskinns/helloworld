23ad75b6b2b6ee8e1d9d2413bb27b7cd1a81d90a
juliuskinns <juliuskinns@gmail.com>
2021-02-18 16:08:01 +0300

    initial commit

---
+++ price.py
@@ -0,0 +1,4 @@
+price = int(1000)
+vat = float(0.16)
+vatinclusive = (price)*float(vat) + price
+print(vatinclusive)
---
+++ print.py
@@ -0,0 +1,4 @@
+age = 20
+fName = " Sam "
+sName = " Tomashi "
+print ("Hello," + fName + str(age))
