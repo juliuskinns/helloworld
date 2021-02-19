585d840e909fc5a8a7e4ed2b44248238ca94effe
juliuskinns <juliuskinns@gmail.com>
2021-02-19 16:50:08 +0300

    initial commit

---
+++ ticket.py
@@ -0,0 +1,18 @@
+ticket = int(200)
+age = input ("enter age")
+if not ("age >= 18"):
+    print ("ok")
+else:
+    print("not ok")
+if ("age = 18"):
+    print ("ok")
+else:
+    print("not ok")
+if ("age < 18"):
+    print ("not ok")
+else:
+    print("ok")
+price = int(200)
+money = input ("enter money")
+balance = ("money") - price
+print(balance)
